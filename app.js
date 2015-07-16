425c0d5c6deb8334c51784b186f6ad7335fee3cf
Sam Stowers <phantom4d@gmail.com>
2015-07-15 21:30:59 -0500

    Add test email code
    Also added sendgrid dependency to package.json

--- app.js
+++ app.js
@@ -58,3 +58,19 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+// Begin SendGrid code
+var sendgrid  = require('sendgrid')('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');
+
+// Sending a test email
+var payload   = {
+  to      : 'phantom4d@gmail.com',
+  from    : 'whyhellothere@gmail.com',
+  subject : 'Testing',
+  text    : 'This is my first email through SendGrid. It will not work.'
+}
+
+sendgrid.send(payload, function(err, json) {
+  if (err) { console.error(err); }
+  console.log(json);
+});
