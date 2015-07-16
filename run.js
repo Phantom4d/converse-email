9546d5bc68bf05a286d32e15c9bf65596807e297
Sam Stowers <phantom4d@gmail.com>
2015-07-15 22:50:49 -0500

    Updated required packages to include Firebase

--- run.js
+++ run.js
@@ -1,6 +1,8 @@
 // Begin SendGrid code
 var sendgrid  = require('sendgrid')('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');
 
+
+/*
 // Sending a test email
 var payload   = {
   to      : 'phantom4d@gmail.com',
@@ -13,3 +15,13 @@ sendgrid.send(payload, function(err, json) {
   if (err) { console.error(err); }
   console.log(json);
 });
+*/
+
+// Require Firebase
+var Firebase  = require('firebase');
+// Firebase references
+var ref = new Firebase("https://goconverse.firebaseio.com");
+var conversationsRef = new Firebase("https://goconverse.firebaseio.com/conversations/");
+var usersRef = new Firebase("https://goconverse.firebaseio.com/users/");
+
+// conversationsRef.on('child_added')
